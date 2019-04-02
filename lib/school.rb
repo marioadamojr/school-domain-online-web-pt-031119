b67434f7b33ece1cdf61bedeb4fb08b5b8f2d1c5
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-02 01:10:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, student_grade)
+    @roster << student_name
+  end
+
+end
