43d7453c3e1ce7c70fc8ee0e60e622c2d9f80bae
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-02 01:13:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ class School
   end
 
   def add_student(student_name, student_grade)
-    @roster[grade] = []
-    @roster[grade] << student_name
+    @roster[student_grade] ||= []
+    @roster[student_grade] << student_name
   end
 
 end
